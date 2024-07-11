e66827dd32a61516bf955af187bc094e249def3e
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-11 18:12:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+##配置代理
+export http_proxy=http://$username:$pwd@$url:$port
 bash <(curl -sLk https://s.dekube.ai/dekube+client/scripts/DEKUBE_1.0.3.sh)
 dekube register $login_key
 cd dekube-client
